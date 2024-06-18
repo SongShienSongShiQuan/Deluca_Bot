3196d6eceb244cb2d79f05901bb86c3646ba50ef
Fear <96379040+7Future@users.noreply.github.com>
2024-06-18 09:52:05 +0500

    Add new files and folders

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.messages = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run(' ADD THE BOT TOKEN HERE ASU')
